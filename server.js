6794d3e07324a6b803f05019aaa87824407aa0af
Mahdi <mahdi@ALFAs-MacBook-Pro.local>
2023-12-11 17:00:41 +0330

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    cookieSession({
+        name: "ticket-session",
+        keys: ["COOKIE_SECRET"], // should use as secret environment variable
+        httpOnly: true
+    })
+);
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to ticket application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
