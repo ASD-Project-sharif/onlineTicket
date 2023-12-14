ef208e37f65ccd4d82164ae8781300a61a3f58cd
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2023-12-14 15:14:22 +0330

    Add routes requirements to server.
    Co-authored-by: yasamingol <yasamingolzar.ce@gmail.com>

--- server.js
+++ server.js
@@ -82,6 +82,10 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to ticket application."});
 });
 
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
