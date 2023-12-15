08e7ec525e1c7b3553a9a3ccd0679602dc7adee1
hoseinaghaei <h.aghaei.araei@gmail.com>
2023-12-15 22:25:33 +0330

    change project structure to /src

--- src/models/role.model.js
+++ src/models/role.model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const Role = mongoose.model(
-    "Role",
+   "Role",
     new mongoose.Schema({
-        name: String
-    })
+       name: String
+   })
 );
 
 module.exports = Role;
