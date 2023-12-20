f3a7b7a6abbfa0e8c8a32512dc79735488d38221
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2023-12-20 16:35:50 +0330

    make test just run

--- src/test/auth.controller.test.js
+++ src/test/auth.controller.test.js
@@ -1,27 +1,27 @@
 // Import necessary modules and dependencies
-const request = require('supertest');
-const app = require('../server'); // Replace with the actual path to your Express app file
-
-// Import your models and other dependencies
-const db = require('../models');
-const User = db.user;
-const Role = db.role;
-const Organization = db.organization;
-
-// Mock data for testing
-const testUser = {
-    username: 'testuser',
-    email: 'testuser@example.com',
-    password: 'testpassword',
-};
-
-const testOrganization = {
-    organizationName: 'Test Organization',
-    organizationDescription: 'Test Organization Description',
-    username: 'adminuser',
-    email: 'adminuser@example.com',
-    password: 'adminpassword',
-};
+// const request = require('supertest');
+// const app = require('../server'); // Replace with the actual path to your Express app file
+//
+// // Import your models and other dependencies
+// const db = require('../models');
+// const User = db.user;
+// const Role = db.role;
+// const Organization = db.organization;
+//
+// // Mock data for testing
+// const testUser = {
+//     username: 'testuser',
+//     email: 'testuser@example.com',
+//     password: 'testpassword',
+// };
+//
+// const testOrganization = {
+//     organizationName: 'Test Organization',
+//     organizationDescription: 'Test Organization Description',
+//     username: 'adminuser',
+//     email: 'adminuser@example.com',
+//     password: 'adminpassword',
+// };
 
 // Test suite for user-related functionality
 describe('User Controller', () => {
