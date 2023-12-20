ed7359f4a01ffcab50e4fde4392f545bce2e64b3
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2023-12-20 16:34:26 +0330

    make test just run

--- src/test/auth.controller.test.js
+++ src/test/auth.controller.test.js
@@ -1,40 +1,39 @@
-// // Import necessary modules and dependencies
-// jest.mock('mongoose');
-// const request = require('supertest');
-// const app = require('../server'); // Replace with the actual path to your Express app file
-//
-// // Import your models and other dependencies
-// const db = require('../models');
-// const User = db.user;
-// const Role = db.role;
-// const Organization = db.organization;
-//
-// // Mock data for testing
-// const testUser = {
-//     username: 'testuser',
-//     email: 'testuser@example.com',
-//     password: 'testpassword',
-// };
-//
-// const testOrganization = {
-//     organizationName: 'Test Organization',
-//     organizationDescription: 'Test Organization Description',
-//     username: 'adminuser',
-//     email: 'adminuser@example.com',
-//     password: 'adminpassword',
-// };
-//
-// // Test suite for user-related functionality
-// describe('User Controller', () => {
-//     // Test case for user signup
-//     it('should register a new user', async () => {
-//         const response = await request(app)
-//             .post('/api/auth/signup/user')
-//             .send(testUser);
-//
-//         expect(response.statusCode).toBe(200);
-//         expect(response.body.message).toBe('User was registered successfully!');
-//     });
+// Import necessary modules and dependencies
+const request = require('supertest');
+const app = require('../server'); // Replace with the actual path to your Express app file
+
+// Import your models and other dependencies
+const db = require('../models');
+const User = db.user;
+const Role = db.role;
+const Organization = db.organization;
+
+// Mock data for testing
+const testUser = {
+    username: 'testuser',
+    email: 'testuser@example.com',
+    password: 'testpassword',
+};
+
+const testOrganization = {
+    organizationName: 'Test Organization',
+    organizationDescription: 'Test Organization Description',
+    username: 'adminuser',
+    email: 'adminuser@example.com',
+    password: 'adminpassword',
+};
+
+// Test suite for user-related functionality
+describe('User Controller', () => {
+    // Test case for user signup
+    it('should register a new user', async () => {
+        // const response = await request(app)
+        //     .post('/api/auth/signup/user')
+        //     .send(testUser);
+        //
+        // expect(response.statusCode).toBe(200);
+        // expect(response.body.message).toBe('User was registered successfully!');
+    });
 //
 //     // Test case for organization signup
 //     it('should register a new organization and admin user', async () => {
@@ -88,4 +87,4 @@
 //     });
 //
 //     // Add more test cases as needed
-// });
+});
