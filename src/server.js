bf7ae2900765b3afb581d8f33c7febdb18467b29
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2023-12-28 01:51:06 +0330

    fix url

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ const Role = db.role;
 
 const setup = async () => {
     try {
-        await db.mongoose.connect(`mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?authSource=admin`, {
+        await db.mongoose.connect(`mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
