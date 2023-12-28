8a65d8bbb8364f08f2a76e2a3d668da344c47cd3
hoseinaghaei <h.aghaei.araei@gmail.com>
2023-12-28 20:04:14 +0330

    fix connection url to mongo.

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ const Role = db.role;
 
 const setupDatabase = async () => {
     try {
-        const dbUrl =`mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}}`;
+        const dbUrl =`mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
         await db.mongoose.connect(dbUrl, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
