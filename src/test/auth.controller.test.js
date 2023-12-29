82b3bc5cb49f990eb16e82a25e76dfca2773d174
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2023-12-29 11:27:46 +0330

    add test for auth controller

--- src/test/auth.controller.test.js
+++ src/test/auth.controller.test.js
@@ -1,90 +1,108 @@
-// Import necessary modules and dependencies
-// const request = require('supertest');
-// const app = require('../server'); // Replace with the actual path to your Express app file
-//
-// // Import your models and other dependencies
-// const db = require('../models');
-// const User = db.user;
-// const Role = db.role;
-// const Organization = db.organization;
-//
-// // Mock data for testing
-// const testUser = {
-//     username: 'testuser',
-//     email: 'testuser@example.com',
-//     password: 'testpassword',
-// };
-//
-// const testOrganization = {
-//     organizationName: 'Test Organization',
-//     organizationDescription: 'Test Organization Description',
-//     username: 'adminuser',
-//     email: 'adminuser@example.com',
-//     password: 'adminpassword',
-// };
+const {createDocument, findOneDocument} = require('../dataAccess/dataAccess');
 
-// Test suite for user-related functionality
-describe('User Controller', () => {
-    // Test case for user signup
-    it('should register a new user', async () => {
-        // const response = await request(app)
-        //     .post('/api/auth/signup/user')
-        //     .send(testUser);
-        //
-        // expect(response.statusCode).toBe(200);
-        // expect(response.body.message).toBe('User was registered successfully!');
-    });
-//
-//     // Test case for organization signup
-//     it('should register a new organization and admin user', async () => {
-//         const response = await request(app)
-//             .post('/api/auth/signup/organization')
-//             .send(testOrganization);
-//
-//         expect(response.statusCode).toBe(200);
-//         expect(response.body.message).toBe('Admin was registered successfully!');
-//     });
-//
-//     // Test case for user signin
-//     it('should signin a user with valid credentials', async () => {
-//         const response = await request(app)
-//             .post('/api/auth/signin')
-//             .send({
-//                 username: testUser.username,
-//                 password: testUser.password,
-//             });
-//
-//         expect(response.statusCode).toBe(200);
-//         expect(response.body.accessToken).toBeDefined();
-//         expect(response.body.username).toBe(testUser.username);
-//     });
-//
-//     // Add more test cases as needed
-// });
-//
-// // Test suite for database interactions
-// describe('Database Interactions', () => {
-//     // Test case for saving a new user to the database
-//     it('should save a new user to the database', async () => {
-//         const newUser = new User(testUser);
-//         await newUser.save();
-//
-//         const savedUser = await User.findOne({username: testUser.username});
-//         expect(savedUser).toBeDefined();
-//         expect(savedUser.username).toBe(testUser.username);
-//     });
-//
-//     // Test case for saving a new organization to the database
-//     it('should save a new organization to the database', async () => {
-//         const newOrganization = new Organization(testOrganization);
-//         await newOrganization.save();
-//
-//         const savedOrganization = await Organization.findOne({
-//             name: testOrganization.organizationName,
-//         });
-//         expect(savedOrganization).toBeDefined();
-//         expect(savedOrganization.name).toBe(testOrganization.organizationName);
-//     });
-//
-//     // Add more test cases as needed
+const AuthControllers = require("../controllers/auth.controller")
+const bcrypt = require("bcryptjs");
+const UserRole = require('../models/userRoles');
+
+
+jest.mock('../dataAccess/dataAccess');
+
+
+beforeEach(() => {
+    jest.clearAllMocks();
 });
+
+describe('Authentication Controllers', () => {
+    test('signup should register a new user successfully', async () => {
+        jest.spyOn(bcrypt, 'hashSync').mockImplementation((pass, salt, cb) => "12345")
+        const userMockData = {
+            username: 'testuser',
+            name: 'Test User',
+            email: 'testuser@example.com',
+            password: 'testpassword',
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(), // This line
+            send: jest.fn(), // also mocking for send function
+        };
+
+
+        createDocument.mockResolvedValueOnce();
+
+        await AuthControllers.signup({body: userMockData}, res);
+
+        expect(createDocument).toHaveBeenCalledWith('User', {...userMockData, password: "12345", role: "user"});
+        // expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith({message: 'User was registered successfully!'});
+    });
+
+    test('signupOrganization should register a new organization and admin successfully', async () => {
+        const organizationMockData = {
+            organizationName: 'TestOrg',
+            organizationDescription: 'Test Organization Description',
+        };
+
+        const adminUserMockData = {
+            username: 'testadmin',
+            email: 'testadmin@example.com',
+            password: 'testpassword',
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(), // This line
+            send: jest.fn(), // also mocking for send function
+        };
+
+        createDocument.mockResolvedValueOnce({_id: 'orgId'});
+        createDocument.mockResolvedValueOnce();
+
+        await AuthControllers.signupOrganization({body: {...organizationMockData, ...adminUserMockData}}, res);
+
+        expect(createDocument).toHaveBeenCalledWith('Organization', {
+            "description": "Test Organization Description",
+            "name": "TestOrg"
+        });
+        expect(createDocument).toHaveBeenCalledWith('User', {
+            ...adminUserMockData,
+            organization: 'orgId',
+            role: UserRole.ADMIN,
+            password: "12345"
+        });
+        // expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith({message: 'Admin was registered successfully!'});
+    });
+
+    test('signin should sign in a user and return a valid JWT token', async () => {
+        jest.spyOn(bcrypt, 'compareSync').mockImplementation((pass, salt, cb) => true)
+
+        const existingUserMockData = {
+            username: 'testuser',
+            password: 'testpassword',
+        };
+
+        const res = mockResponse();
+
+        findOneDocument.mockResolvedValueOnce({
+            _id: 'userId',
+            username: 'testuser',
+            email: 'testuser@example.com',
+            password: 'hashedPassword',
+            role: UserRole.USER,
+        });
+
+        await AuthControllers.signin({body: existingUserMockData}, res);
+
+        expect(findOneDocument).toHaveBeenCalledWith('User', {username: 'testuser'});
+        expect(res.status).toHaveBeenCalledWith(200);
+        // Add more assertions based on your specific requirements
+    });
+});
+
+// Mock response object
+function mockResponse() {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.send = jest.fn().mockReturnValue(res);
+    return res;
+}
