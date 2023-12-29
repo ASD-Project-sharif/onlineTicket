be1d7700958493c8190ed7b30a737f7d5cd08df2
hoseinaghaei <h.aghaei.araei@gmail.com>
2023-12-29 22:00:55 +0330

    refactor: remove redundant try-catch

--- src/dataAccess/dataAccess.js
+++ src/dataAccess/dataAccess.js
@@ -1,79 +1,48 @@
 const mongoose = require('mongoose');
+const mongooseClient = mongoose.Mongoose;
 
 async function createDocument(modelName, data) {
-    try {
-        const Model = mongoose.model(modelName);
-        const newDocument = new Model(data);
-        const savedDocument = await newDocument.save();
-        return savedDocument;
-    } catch (error) {
-        throw error;
-    }
+    const Model = mongoose.model(modelName);
+    const newDocument = new Model(data);
+    return await newDocument.save();
 }
 
 async function getAllDocuments(modelName) {
-    try {
-        const Model = mongoose.model(modelName);
-        const documents = await Model.find();
-        return documents;
-    } catch (error) {
-        throw error;
-    }
+    const Model = mongoose.model(modelName);
+    return Model.find();
 }
 
 async function getDocumentById(modelName, id) {
-    try {
-        const Model = mongoose.model(modelName);
-        const document = await Model.findById(id);
-        return document;
-    } catch (error) {
-        throw error;
-    }
+    const Model = mongoose.model(modelName);
+    return Model.findById(id);
 }
 
 async function updateDocumentById(modelName, id, data) {
-    try {
-        const Model = mongoose.model(modelName);
-        const updatedDocument = await Model.findByIdAndUpdate(id, data, {
-            new: true,
-            runValidators: true,
-        });
-        return updatedDocument;
-    } catch (error) {
-        throw error;
-    }
+    const Model = mongoose.model(modelName);
+    return Model.findByIdAndUpdate(id, data, {
+        new: true,
+        runValidators: true,
+    });
 }
 
 async function deleteDocumentById(modelName, id) {
-    try {
-        const Model = mongoose.model(modelName);
-        const deletedDocument = await Model.findByIdAndDelete(id);
-        return deletedDocument;
-    } catch (error) {
-        throw error;
-    }
+    const Model = mongoose.model(modelName);
+    return Model.findByIdAndDelete(id);
 }
 
 async function countDocuments(modelName) {
-    try {
-        const Model = mongoose.model(modelName);
-        return Model.estimatedDocumentCount();
-    } catch (error) {
-        throw error;
-    }
+    const Model = mongoose.model(modelName);
+    return Model.estimatedDocumentCount();
 }
 
 async function findOneDocument(modelName, query) {
-    try {
-        const Model = mongoose.model(modelName);
-        const document = await Model.findOne(query).exec();
-        return document;
-    } catch (error) {
-        throw error;
-    }
+    const Model = mongoose.model(modelName);
+    const document = await Model.findOne(query).exec();
+    return document;
 }
 
 module.exports = {
+    mongooseClient,
     createDocument,
     getAllDocuments,
     getDocumentById,
