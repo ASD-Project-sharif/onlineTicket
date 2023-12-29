0140815058f53fb28d478a661cd7963ab83c5a5a
hoseinaghaei <h.aghaei.araei@gmail.com>
2023-12-29 22:05:18 +0330

    refactor: separate duties in server.js

--- src/server.js
+++ src/server.js
@@ -1,44 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
-
+const sanitizeUserInput = require("./middlewares/xss.middleware")
+const errorHandler = require("./middlewares/errors.middleware")
 const app = express();
-
-const corsOpts = {
-    origin: '*',
-
-    methods: [
-        'GET',
-        'POST',
-    ],
-
-    allowedHeaders: [
-        'Content-Type',
-    ],
-};
-
-app.use(cors(corsOpts));
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({extended: true}));
-
-app.use(
-    cookieSession({
-        name: "ticket-session",
-        keys: ["COOKIE_SECRET"], // should use as secret environment variable
-        httpOnly: true
-    })
-);
-
 const db = require("./models");
-const {createDocument, countDocuments} = require("./dataAccess/dataAccess");
 
 const setupDatabase = async () => {
     try {
-        const dbUrl =`mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+        const dbUrl = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
         await db.mongoose.connect(dbUrl, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -50,15 +20,48 @@ const setupDatabase = async () => {
     }
 }
 
+function setupUses(app) {
+    app.use(express.json());
+    app.use(sanitizeUserInput);
+    app.use(express.urlencoded({extended: true}));
+    app.use(
+        cookieSession({
+            name: "ticket-session",
+            keys: ["COOKIE_SECRET"], // should use as secret environment variable
+            httpOnly: true
+        })
+    );
+
+
+    const corsOpts = {
+        origin: '*',
+
+        methods: [
+            'GET',
+            'POST',
+        ],
+
+        allowedHeaders: [
+            'Content-Type',
+        ],
+    };
+
+    app.use(cors(corsOpts));
+    app.use(errorHandler)
+}
+
+function setupRoutes(app) {
+    app.get("/", (req, res) => {
+        res.json({message: "Welcome to ticket application."});
+    });
+    require('./routes/auth.routes')(app);
+    require('./routes/ticket.routes')(app);
+
+}
+
 setupDatabase()
-
-// simple route
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to ticket application."});
-});
-
-require('./routes/auth.routes')(app);
-require('./routes/user.routes')(app);
+setupUses(app);
+setupRoutes(app)
 
 
 // set port, listen for requests
