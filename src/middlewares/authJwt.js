b0121f25d4502e1583750727990c6c4321e96ddf
hoseinaghaei <h.aghaei.araei@gmail.com>
2024-01-28 01:00:31 +0330

    remove unused codes.

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -3,7 +3,6 @@ const UserRepository = require('../repository/user.repository');
 
 verifyToken = (req, res, next) => {
   let token = req.headers['x-access-token'];
-  token = token.replace('Token ', '');
   if (!token) {
     return res.status(403).send({message: 'No token provided!'});
   }
