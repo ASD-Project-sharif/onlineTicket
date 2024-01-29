e8f076bdfd24b4989f9b7d420f79b82aae319f63
hoseinaghaei <h.aghaei.araei@gmail.com>
2024-01-30 00:23:25 +0330

    set deadline to end of the day.

--- src/services/ticket.services.js
+++ src/services/ticket.services.js
@@ -41,7 +41,7 @@ const isInputDataValid = (req, res) => {
       res.status(400).send({message: 'Invalid deadline format'});
       return false;
     }
-    if (deadline <= TimeServices.now) {
+    if (deadline <= TimeServices.now()) {
       res.status(400).send({message: 'Deadline must be after now!!'});
       return false;
     }
